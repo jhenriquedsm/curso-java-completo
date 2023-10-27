bb8de3bd90a45358a41b3bae192b83810db3bef0
jhenriquedsm <jhsilvamata@gmail.com>
2023-10-27 13:53:13 -0300

    estrutura condicional

--- src/estruturaSequencial/exercices/exercise01.java
+++ src/estruturaSequencial/exercices/exercise01.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
 mensagem explicativa, conforme exemplos.
 */
-public class exercice01 {
+public class exercise01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x, y, soma;
