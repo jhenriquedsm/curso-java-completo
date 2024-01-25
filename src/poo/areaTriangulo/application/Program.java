70e5961f5415774472bed2f0f3c4084ae45690cf
jhenriquedsm <jhsilvamata@gmail.com>
2024-01-24 23:34:55 -0300

    implementação de método na classe Triangle para reaproveitamento de código e delegação de responsabilidade

--- src/poo/areaTriangulo/application/Program.java
+++ src/poo/areaTriangulo/application/Program.java
@@ -5,7 +5,6 @@ import poo.areaTriangulo.entities.Triangle;
 import java.util.Locale;
 import java.util.Scanner;
 
-// solução sem orientação a objeto
 public class Program {
     public static void main(String[] args) {
 
@@ -26,11 +25,8 @@ public class Program {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-
-        p = (y.a + y.b + y.c) / 2.0;
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+        double areaX = x.area();
+        double areaY = y.area();
 
         System.out.printf("Triangle X area: %.4f%n", areaX);
         System.out.printf("Triangle Y area: %.4f%n", areaY);
