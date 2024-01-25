064671406deaf1dc626545b0a97b626aa672fca4
jhenriquedsm <jhsilvamata@gmail.com>
2024-01-25 19:15:16 -0300

    exercise 02 - Employee

---
+++ src/poo/exercises/exercise2/application/Program.java
@@ -0,0 +1,34 @@
+package poo.exercises.exercise2.application;
+
+import poo.exercises.exercise2.entities.Employee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Employee employee = new Employee();
+        System.out.print("Name: ");
+        employee.name = sc.nextLine();
+        System.out.print("Gross salary: ");
+        employee.grossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        employee.tax = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Employee: " + employee);
+
+        System.out.println();
+        System.out.print("Which percentage to increase salary? ");
+        double porcentage = sc.nextDouble();
+        employee.increaseSalary(porcentage);
+
+        System.out.println();
+        System.out.println("Updated data: " + employee);
+
+        sc.close();
+    }
+}
---
+++ src/poo/exercises/exercise2/entities/Employee.java
@@ -0,0 +1,20 @@
+package poo.exercises.exercise2.entities;
+
+public class Employee {
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+    public double netSalary(){
+        return this.grossSalary - this.tax;
+    }
+
+    public void increaseSalary(double percentage){
+        this.grossSalary += (this.grossSalary * percentage) / 100.0;
+    }
+
+    @Override
+    public String toString(){
+       return name + ", $ " + String.format("%.2f", netSalary());
+    }
+}
