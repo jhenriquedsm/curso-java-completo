7ac55bfaa86dea5be3f65d5da2811e35842328d9
jhenriquedsm <jhsilvamata@gmail.com>
2024-01-31 15:31:23 -0300

    versão 3: classe Calculator com método estático

--- src/poo/membrosEstaticos/util/Calculator.java
+++ src/poo/membrosEstaticos/util/Calculator.java
@@ -1,13 +1,13 @@
 package poo.membrosEstaticos.util;
 
 public class Calculator {
-    public final double PI = 3.14159;
+    public static final double PI = 3.14159;
 
-    public double circumference(double radius){
+    public static double circumference(double radius){
         return 2.0 * PI * radius;
     }
 
-    public double volume(double radius){
+    public static double volume(double radius){
         return 4.0 * PI * radius * radius * radius / 3.0;
     }
 }
