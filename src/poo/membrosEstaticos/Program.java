17509c5db6f53069d02a60b45a08325ba3d28b8f
jhenriquedsm <jhsilvamata@gmail.com>
2024-01-31 15:26:01 -0300

    versão 2: classe Calculator com membros de instância

--- src/poo/membrosEstaticos/Program.java
+++ src/poo/membrosEstaticos/Program.java
@@ -1,36 +1,27 @@
 package poo.membrosEstaticos;
 
+import poo.membrosEstaticos.util.Calculator;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-
-    public static final double PI = 3.14159;
-
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        Calculator calc = new Calculator();
 
         System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = circumference(radius);
+        double c = calc.circumference(radius);
 
-        double v = volume(radius);
+        double v = calc.volume(radius);
 
         System.out.printf("Circumference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI value: %.2f%n", PI);
+        System.out.printf("PI value: %.2f%n", calc.PI);
 
         sc.close();
     }
-
-    public static double circumference(double radius){
-        return 2.0 * PI * radius;
-    }
-
-    public static double volume(double radius){
-        return 4.0 * PI * radius * radius * radius / 3.0;
-    }
-
 }
