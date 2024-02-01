b512eb6c415734286e8854bc31165d11ce341be6
jhenriquedsm <jhsilvamata@gmail.com>
2024-02-01 14:39:00 -0300

    implementando Construtor na classe Product

--- src/poo/produto/application/Program.java
+++ src/poo/produto/application/Program.java
@@ -10,22 +10,22 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
-
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Price: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
         System.out.print("Quantity: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity);
 
         System.out.println();
         System.out.println("Product Data: " + product);
 
         System.out.println();
         System.out.print("Enter the number of products to be added in stock: ");
-        int quantity = sc.nextInt();
+        quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
