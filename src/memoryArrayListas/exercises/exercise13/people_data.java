43cd43c59b0c5d8f5aae6420c1c78cc97de9f612
jhenriquedsm <jhsilvamata@gmail.com>
2024-03-31 23:22:20 -0300

    update

--- src/memoryArrayListas/exercises/exercise13/people_data.java
+++ src/memoryArrayListas/exercises/exercise13/people_data.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 /*
 Tem-se um conjunto de dados contendo a altura e o gênero (M, F) de N pessoas. Fazer um programa
 que calcule e escreva a maior e a menor altura do grupo, a média de altura das mulheres, e o número
-de homens. 
+de homens.
 */
 
 public class people_data {
