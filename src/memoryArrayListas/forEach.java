ebccc1a0f20023866719bdeb5abd7ad7ad41ee35
jhenriquedsm <jhsilvamata@gmail.com>
2024-04-03 21:40:08 -0300

    for each in java

---
+++ src/memoryArrayListas/forEach.java
@@ -0,0 +1,17 @@
+package memoryArrayListas;
+
+public class forEach {
+    public static void main(String[] args) {
+
+        String[] vect = new String[] {"Maria", "Bob", "Alex"};
+
+        for (int i = 0; i < vect.length; i++){
+            System.out.println(vect[i]);
+        }
+
+        System.out.println("\nfor each:");
+        for (String obj : vect){
+            System.out.println(obj);
+        }
+    }
+}
