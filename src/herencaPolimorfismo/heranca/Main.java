146ae8e6f49874ad8c9678ac69b3d8219bf7e478
jhenriquedsm <jhsilvamata@gmail.com>
2024-09-19 14:55:18 -0300

    upcasting and downcasting

--- src/herencaPolimorfismo/heranca/Main.java
+++ src/herencaPolimorfismo/heranca/Main.java
@@ -2,7 +2,30 @@ package herencaPolimorfismo.heranca;
 
 public class Main {
     public static void main(String[] args) {
-        BusinessAccount ba = new BusinessAccount();
-        ba.getBalance();
+
+        Account acc = new Account(1001, "Alex", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+        // UPCASTING
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.1);
+
+        // DOWNCASTING
+        BusinessAccount bacc1 = (BusinessAccount) acc2;
+        bacc1.loan(100.0);
+
+        // BusinessAccount bacc2 = (BusinessAccount) acc3;
+        if (acc3 instanceof BusinessAccount){
+            BusinessAccount bacc2 = (BusinessAccount) acc3;
+            bacc2.loan(200.0);
+            System.out.println("Loan!");
+        }
+
+        if (acc3 instanceof SavingsAccount){
+            SavingsAccount sacc = (SavingsAccount) acc3;
+            sacc.updateBalance();
+            System.out.println("Update!");
+        }
     }
 }
