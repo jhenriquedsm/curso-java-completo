7300c0c7929de67f00affa8c24667e27595fa664
jhenriquedsm <jhsilvamata@gmail.com>
2024-09-19 12:20:32 -0300

    inheritance

---
+++ src/herencaPolimorfismo/heranca/BusinessAccount.java
@@ -0,0 +1,20 @@
+package herencaPolimorfismo.heranca;
+
+public class BusinessAccount extends Account{
+    private Double loanLimit;
+
+    public BusinessAccount(){
+        super();
+    }
+
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan (double amount){
+        if (amount <= loanLimit){
+            balance += amount - 10.0;
+        }
+    }
+}
---
+++ src/herencaPolimorfismo/heranca/Main.java
@@ -0,0 +1,8 @@
+package herencaPolimorfismo.heranca;
+
+public class Main {
+    public static void main(String[] args) {
+        BusinessAccount ba = new BusinessAccount();
+        ba.getBalance();
+    }
+}
