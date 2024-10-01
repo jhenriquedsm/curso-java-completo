f5f0a01ec0175eea07fa0d4fc7b751bb82a02450
jhenriquedsm <jhsilvamata@gmail.com>
2024-10-01 19:54:22 -0300

    reading text file with File and Scanner

---
+++ src/trabalhandoComArquivos/FileExample.java
@@ -0,0 +1,25 @@
+package trabalhandoComArquivos;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileExample {
+    public static void main(String[] args) {
+
+        File file = new File("C:\\temp\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            if (sc != null){
+                sc.close();
+            }
+        }
+    }
+}
