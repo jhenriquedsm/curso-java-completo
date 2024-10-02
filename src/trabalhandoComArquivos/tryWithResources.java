cf270efbbe0f01fd7f12c9de20001778a2a9a614
jhenriquedsm <jhsilvamata@gmail.com>
2024-10-02 12:14:58 -0300

    try-with-resources

---
+++ src/trabalhandoComArquivos/tryWithResources.java
@@ -0,0 +1,22 @@
+package trabalhandoComArquivos;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class tryWithResources {
+    public static void main(String[] args) {
+        String path = "C:\\Temp\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
