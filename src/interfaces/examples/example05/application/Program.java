bee2bafd42f69125478a0175e83f1fa68e25aefd
jhenriquedsm <jhsilvamata@gmail.com>
2024-10-07 18:27:07 -0300

    solution without interface

---
+++ src/interfaces/examples/example05/application/Program.java
@@ -0,0 +1,27 @@
+package interfaces.examples.example05.application;
+
+import interfaces.examples.example05.services.BrazilianInterestService;
+import interfaces.examples.example05.services.USAInterestService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Amount: ");
+        double amount = sc.nextDouble();
+        System.out.print("Months: ");
+        int months = sc.nextInt();
+
+        USAInterestService is = new USAInterestService(1.0);
+        double payment = is.payment(amount, months);
+
+        System.out.println("Payment after " + months + " months:");
+        System.out.println(String.format("%.2f", payment));
+
+        sc.close();
+    }
+}
---
+++ src/interfaces/examples/example05/services/BrazilianInterestService.java
@@ -0,0 +1,22 @@
+package interfaces.examples.example05.services;
+
+import java.security.InvalidParameterException;
+
+public class BrazilianInterestService {
+    private Double interestRate;
+
+    public BrazilianInterestService(Double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public Double getInterestRate() {
+        return interestRate;
+    }
+
+    public double payment(double amount, int months){
+        if (months < 1){
+            throw new InvalidParameterException("Months must be greater than zero");
+        }
+        return amount * Math.pow(1.0 + interestRate / 100.0, months); //potenciação
+    }
+}
