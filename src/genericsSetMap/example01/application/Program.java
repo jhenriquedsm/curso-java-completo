2151dab6c5be8381cb4f3ba02fcd9d57007edede
jhenriquedsm <jhsilvamata@gmail.com>
2024-10-09 11:25:31 -0300

    problem without generics

---
+++ src/genericsSetMap/example01/application/Program.java
@@ -0,0 +1,25 @@
+package genericsSetMap.example01.application;
+
+import genericsSetMap.example01.services.PrintService;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PrintService ps = new PrintService();
+
+        System.out.print("How many values: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++){
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First: " + ps.first());
+
+        sc.close();
+    }
+}
