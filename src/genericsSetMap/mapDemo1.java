e370fcf8cb6c729eda16dcba0f0539ef378907f1
jhenriquedsm <jhsilvamata@gmail.com>
2024-10-14 13:25:33 -0300

    map example 1

---
+++ src/genericsSetMap/mapDemo1.java
@@ -0,0 +1,27 @@
+package genericsSetMap;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class mapDemo1 {
+    public static void main(String[] args) {
+
+        Map<String, String> cookies = new TreeMap<>();
+        cookies.put("username", "Maria");
+        cookies.put("email", "maria@email.com");
+        cookies.put("phone", "987654321");
+
+        cookies.remove("email");
+        cookies.put("phone", "321654987"); //sobrescreve
+
+        System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+        System.out.println("Phone number: " + cookies.get("phone"));
+        System.out.println("Email: " + cookies.get("email"));
+        System.out.println("Size: " + cookies.size());
+
+        System.out.println("ALL COOKIES:");
+        for (String key : cookies.keySet()){
+            System.out.println(key + ": " + cookies.get(key));
+        }
+    }
+}
