ca8f1e4b2dd46c5b4b13457e00a1725160f3721c
jhenriquedsm <jhsilvamata@gmail.com>
2024-10-16 20:21:37 -0300

    Comparator - way 2

--- src/programacaoFuncional/example01/application/Program.java
+++ src/programacaoFuncional/example01/application/Program.java
@@ -4,6 +4,7 @@ import programacaoFuncional.example01.entities.MyComparator;
 import programacaoFuncional.example01.entities.Product;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Program {
@@ -14,7 +15,14 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        list.sort(new MyComparator());
+        Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
+            }
+        };
+
+        list.sort(comp);
 
         for (Product p : list){
             System.out.println(p);
