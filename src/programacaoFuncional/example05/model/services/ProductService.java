d03dc88e6f98dd6d19161959a025af360e28a301
jhenriquedsm <jhsilvamata@gmail.com>
2024-12-11 15:17:29 -0300

    function that receives function as parameter

---
+++ src/programacaoFuncional/example05/model/services/ProductService.java
@@ -0,0 +1,18 @@
+package programacaoFuncional.example05.model.services;
+
+import programacaoFuncional.example05.entities.Product;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ProductService {
+    public double filteredSum(List<Product> list, Predicate<Product> criteria){
+        double sum = 0.0;
+        for (Product p : list){
+            if (criteria.test(p)){
+                sum += p.getPrice();
+            }
+        }
+        return sum;
+    }
+}
