6eadb9cbdf8e1702e960ab4ea9c0eefd08165dfa
jhenriquedsm <jhsilvamata@gmail.com>
2024-12-11 14:12:38 -0300

    lambda expression declared

--- src/programacaoFuncional/example03/application/Program.java
+++ src/programacaoFuncional/example03/application/Program.java
@@ -6,6 +6,7 @@ import programacaoFuncional.example03.util.PriceUpdate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 public class Program {
     public static void main(String[] args) {
@@ -17,7 +18,10 @@ public class Program {
         list.add(new Product("Tablet", 350.00));
         list.add(new Product("HD Case", 80.90));
 
-        list.forEach(Product::nonStaticPriceUpdate);
+        Consumer<Product> consumer = p -> p.setPrice(p.getPrice() * 1.1);
+        list.forEach(consumer);
+
+        //list.forEach(Product::nonStaticPriceUpdate);
 
         list.forEach(System.out::println);
     }
