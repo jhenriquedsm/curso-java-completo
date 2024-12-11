1ac07055169e2600cd7f0be187abd717f75e1393
jhenriquedsm <jhsilvamata@gmail.com>
2024-12-11 14:53:06 -0300

    lambda expression inline - Function

--- src/programacaoFuncional/example04/application/Program.java
+++ src/programacaoFuncional/example04/application/Program.java
@@ -19,9 +19,7 @@ public class Program {
         list.add(new Product("Tablet", 350.00));
         list.add(new Product("HD Case", 80.90));
 
-        Function<Product, String> function = p -> p.getName().toUpperCase();
-
-        List<String> names = list.stream().map(function).collect(Collectors.toList());
+        List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
         names.forEach(System.out::println);
     }
 }
