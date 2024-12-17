26fac9b863fa2c3589acd17c94c4cae8daa136b8
jhenriquedsm <jhsilvamata@gmail.com>
2024-12-17 15:13:03 -0300

    add in.txt used in the exercise

--- src/programacaoFuncional/exercises/exercise01/application/Program.java
+++ src/programacaoFuncional/exercises/exercise01/application/Program.java
@@ -6,6 +6,14 @@ arquivo em formato .csv (suponha que exista pelo menos um produto).
 Em seguida mostrar o preço médio dos produtos. Depois, mostrar os
 nomes, em ordem decrescente, dos produtos que possuem preço
 inferior ao preço médio.
+
+in.txt:
+Tv,900.00
+Mouse,50.00
+Tablet,350.50
+HD Case,80.90
+Computer,850.00
+Monitor,290.00
 */
 
 import programacaoFuncional.exercises.exercise01.entities.Product;
